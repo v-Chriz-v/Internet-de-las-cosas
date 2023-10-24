80ce28d56827acc3380996b6d7d9b11bb46580d5
MarcoAntonioAlfaro <190300374@ucaribe.edu.mx>
2023-10-23 19:27:13 -0500

    Modificacion a Sensor GPS

--- ubicacion_GPS.py
+++ ubicacion_GPS.py
@@ -1,14 +1,41 @@
-#Simulacion de un GPS
-import random
 import time
+import random
 
-print("Iniciando simuacion de GPS...")
+# Coordenadas iniciales
+latitud = 21.200642044259617
+longitud = -86.8233056004058
 
-try:
-    while True:
-        latitude = random.uniform(-90, 90)
-        longitude = random.uniform(-180, 180)
-        print(f"Latitud: {latitude}, Longitud: {longitude}")
-        time.sleep(1)  # Emula la actualización periódica del GPS
-except KeyboardInterrupt:
-    print("Simulacion de GPS finalizada")
+# Velocidad del tren en metros por segundo
+velocidad = 10.0  # 10 metros por segundo (ajusta según sea necesario)
+
+# Dirección inicial (en grados, 0: Este, 90: Norte, 180: Oeste, 270: Sur)
+direccion = 0
+
+# Función para simular el movimiento del tren
+def mover_tren():
+    global latitud, longitud, direccion
+
+    # Simula un cambio en la dirección (puedes ajustar esto)
+    if random.random() < 0.1:  # Cambio de dirección con una probabilidad del 10%
+        direccion = random.choice([0, 90, 180, 270])
+
+    # Calcula el cambio en las coordenadas basado en la velocidad
+    if direccion == 0:
+        longitud += (velocidad / 100000)  # 100,000 metros en 1 grado de longitud aproximadamente
+    elif direccion == 90:
+        latitud += (velocidad / 100000)  # 100,000 metros en 1 grado de latitud aproximadamente
+    elif direccion == 180:
+        longitud -= (velocidad / 100000)
+    elif direccion == 270:
+        latitud -= (velocidad / 100000)
+
+    print(f"Latitud: {latitud}, Longitud: {longitud}")
+
+    time.sleep(1)  # Tiempo de espera para simular el movimiento
+
+if __name__ == "__main__":
+    try:
+        while True:
+            mover_tren()
+    except KeyboardInterrupt:
+        print("Simulacion Terminada")
