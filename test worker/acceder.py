663766ea63d01101328cd5e26b7d3d21c6e67de0
MarcoAntonioAlfaro <190300374@ucaribe.edu.mx>
2023-10-25 17:54:07 -0500

    Pruebas de SQLite (exitoso)

---
+++ test worker/acceder.py
@@ -0,0 +1,29 @@
+import sqlite3
+from PIL import Image
+
+# Conectarse a la base de datos
+conn = sqlite3.connect("mi_base_de_datos.db")
+
+# Crear un objeto cursor
+cursor = conn.cursor()
+
+# Realizar una consulta para recuperar datos
+cursor.execute("SELECT * FROM Datos")
+
+# Obtener los resultados de la consulta
+resultados = cursor.fetchall()
+
+# Procesar los resultados
+for fila in resultados:
+    tiempo, ubicacion, vibracion, captura = fila
+    print(f"Tiempo: {tiempo}")
+    print(f"Ubicación: {ubicacion}")
+    print(f"Vibración: {vibracion}")
+    # Guardar el BLOB en un archivo
+    nombre_archivo = "imagen_recuperada.png"
+    with open(nombre_archivo, "wb") as archivo_salida:
+        archivo_salida.write(captura)
+    imagen = Image.open(nombre_archivo)
+    imagen.show()
+# Cerrar la conexión
+conn.close()
---
+++ test worker/creacion_sqlite.py
@@ -0,0 +1,39 @@
+import sqlite3
+import os
+
+database_name = "mi_base_de_datos.db"
+
+# 1. Conectar o crear una base de datos
+conn = sqlite3.connect(database_name)
+
+# 2. Crear un objeto cursor
+cursor = conn.cursor()
+
+# 3. Crear una tabla para almacenar los datos
+
+if os.path.exists(database_name):
+    print(f"La base de datos '{database_name}' ya existe.")
+else:
+    print(f"La base de datos '{database_name}' no existe. Creando base de datos")
+    cursor.execute('''CREATE TABLE Datos (
+                  Tiempo DATETIME,
+                  Ubicacion TEXT,
+                  Vibracion REAL,
+                  Captura BLOB
+                  )''')
+
+# 4. Insertar datos en la tabla
+tiempo = "2023-10-25 15:30:00"  # Ejemplo de fecha y hora
+ubicacion = "Latitud: 123.456, Longitud: -78.901"  # Ejemplo de ubicación
+vibracion = 0.123  # Ejemplo de valor de vibración
+# Ejemplo de captura de imagen en formato binario (debes adaptar esta parte a tu caso)
+with open("image.png", "rb") as imagen_file:
+    captura = imagen_file.read()
+
+# Insertar un registro en la tabla
+cursor.execute("INSERT INTO Datos (Tiempo, Ubicacion, Vibracion, Captura) VALUES (?, ?, ?, ?)",
+               (tiempo, ubicacion, vibracion, captura))
+
+# Guardar los cambios y cerrar la conexión
+conn.commit()
+conn.close()
