6ac9989e95795afa81e2ad206b100b787f73e0a9
Angel Daniel <200300611@ucaribe.edu.mx>
2023-10-30 20:18:56 -0500

    api

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -1,46 +1,91 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 import json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
+import sqlite3
+from datetime import datetime
+
+#Conexion de la API a la base de datos
+#La base de datos MySQL
+#Se usa pymysql
+app = Flask(__name__)
+db_name = "basededatos.db" 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Datos(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    Tiempo = db.Column(db.DateTime)
+    Vibracion = db.Column(db.Float)
+    Latitud = db.Column(db.String)
+    Longitud = db.Column(db.String)
+    Captura = db.Column(db.LargeBinary)
+    Anormal = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'Tiempo': self.Tiempo,  
+            'Vibracion': self.Vibracion,
+            'Latitud': self.Latitud,
+            'Longitud': self.Longitud,
+            'Captura': self.Captura,
+            'Anormal': self.Anormal
+        }
+    
+@app.route('/api/datos', methods=['GET'])
+def get_datos():
+    datos = Datos.query.order_by(Datos.Tiempo).all()
+
+    # Convertir los datos a un formato JSON
+    datos_json = []
+    for i in range(len(datos) - 1):
+        dato_actual = datos[i]
+        dato_siguiente = datos[i + 1]
+
+        tiempo_actual = dato_actual.Tiempo
+        tiempo_siguiente = dato_siguiente.Tiempo
+
+        diferencia_tiempo = (tiempo_siguiente - tiempo_actual).total_seconds()
+
+        if diferencia_tiempo <= 10:
+            datos_json.append(dato_actual.to_dict())
+
+    return jsonify(datos_json)
 
 
-db = SQLAlchemy()
+@app.route('/api/datos/fecha', methods=['GET'])
+def obtenerDatosFecha():
+    fecha = request.args.get('fecha', default = '', type = str)
+    conn = sqlite3.connect('./test_worker/basededatos.db')
+    cursor = conn.cursor()
 
-def create_app():
-    app = Flask(__name__)
-    db_name = "datashm" 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/' + db_name
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    db.init_app(app)
-    return app
+    cursor.execute("SELECT * FROM Datos WHERE DATE(Tiempo) = DATE(?)", (fecha,))
+    datos = cursor.fetchall()
 
-app = create_app()
+    if len(datos) == 0:
+        abort(404, description="No se encontraron datos para la fecha especificada")
 
+    # Convertir los datos a un formato JSON
+    datos_json = []
+    for dato in datos:
+        dato_dict = {
+            'id': dato[0],
+            'Tiempo': dato[1],
+            'Vibracion': dato[2],
+            'Latitud': dato[3],
+            'Longitud': dato[4],
+            'Captura': dato[5],
+            'Anormal': dato[6]
+        }
+        datos_json.append(dato_dict)
 
-# Ruta para obtener registros desde el archivo JSON
-#El archivo lo debe de cosneguir desde Rabbit
-@app.route('/registros', methods=['GET'])
-def obtener_registros():
-    try:
-        with open('dataESP/registros.json', 'r') as json_file:
-            registros = json.load(json_file)
-        return jsonify({"registros": registros})
-    except FileNotFoundError:
-        return jsonify({"error": "El archivo de registros no se encuentra"})
-
-@app.route('/test', methods=['GET'])
-def testdb():
-    try:
-        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-        return '<h1>It works.</h1>'
-    except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+    return jsonify(datos_json)
 
 #El API debe hacer una query de los datos hacia el usuario
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+
